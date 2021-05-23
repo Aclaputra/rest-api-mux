4b094c4015033437961178de8b0e41f02840562c
Acla Putra <aclalead@gmail.com>
2021-05-23 21:55:53 +0700

    connect database with gorm to mysql server

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+
+	"gorm.io/gorm"
+)
+
+
+var db *gorm.DB
+var err error
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/go_rest_api?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+
+	})
+
+	if err!= nil {
+		log.Println("Connection failed", err)
+	} else {
+		log.Println("Connection established")
+	}
+}
